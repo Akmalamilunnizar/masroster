c85d195556bc8cf4f905d3694a0cf89ac4c1930c
Nomikuza <uzumakisujai@gmail.com>
2026-06-06 17:40:13 +0700

    fix(js modal add produk): clean up template and script files by removing unused Bootstrap links and ensuring proper formatting

--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -11,7 +11,7 @@
     <!-- Custom Modal CSS -->
     <link rel="stylesheet" href="{{ asset('css/custom-modal.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
 
     <title>@yield('page_title')</title>
 
@@ -696,7 +696,6 @@
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     @stack('modals')
     @stack('scripts')
